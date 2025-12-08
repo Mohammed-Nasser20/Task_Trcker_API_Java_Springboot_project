83d5ecefaae2d55ed3a10c6d59a5e72f1b3f5117
Mohammed-Nasser20 <mn4418845@gmail.com>
2025-12-08 14:10:06 +0200

    feat: add endpoint to create a task

--- src/main/java/com/canMe/task_list/controller/TaskController.java
+++ src/main/java/com/canMe/task_list/controller/TaskController.java
@@ -1,18 +1,16 @@
 package com.canMe.task_list.controller;
 
 import com.canMe.task_list.dto.TaskDto;
+import com.canMe.task_list.entity.Task;
 import com.canMe.task_list.mapper.TaskMapper;
 import com.canMe.task_list.service.TaskService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("task_list/{task_list_id}/tasks")
+@RequestMapping("task-lists/{task_list_id}/tasks")
 @AllArgsConstructor
 public class TaskController {
     private final TaskService taskService;
@@ -20,5 +18,13 @@ public class TaskController {
     @GetMapping
     public List<TaskDto> listTasks(@PathVariable("task_list_id") int id) {
         return taskService.listTasks(id).stream().map(taskMapper::toDto).toList();
+    }
+    @PostMapping
+    public TaskDto createTask(
+            @PathVariable("task_list_id") int id,
+            @RequestBody TaskDto taskDto
+    ) {
+        Task task = taskService.createTask(id, taskMapper.fromDto(taskDto));
+        return taskMapper.toDto(task);
     }
 }
--- src/main/java/com/canMe/task_list/service/impl/TaskServiceImpl.java
+++ src/main/java/com/canMe/task_list/service/impl/TaskServiceImpl.java
@@ -1,20 +1,51 @@
 package com.canMe.task_list.service.impl;
 
 import com.canMe.task_list.entity.Task;
+import com.canMe.task_list.entity.TaskList;
+import com.canMe.task_list.entity.TaskPriority;
+import com.canMe.task_list.entity.TaskStatus;
 import com.canMe.task_list.repository.TaskListRepository;
 import com.canMe.task_list.repository.TaskRepository;
 import com.canMe.task_list.service.TaskService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
 public class TaskServiceImpl implements TaskService {
     private final TaskRepository taskRepository;
+    private final TaskListRepository taskListRepository;
     @Override
     public List<Task> listTasks(int taskListId) {
         return taskRepository.findByTaskListId(taskListId);
+    }
+
+    @Override
+    public Task createTask(int taskListId, Task task) {
+        if (task.getId() != null) {
+            throw new IllegalArgumentException("This task is already has ID");
+        }
+        if (task.getTitle() == null || task.getTitle().isBlank()) {
+            throw new IllegalArgumentException("Title must be not null or blank");
+        }
+        TaskPriority taskPriority = Optional.ofNullable(task.getTaskPriority())
+                .orElse(TaskPriority.MEDIUM);
+        TaskStatus taskstatus = TaskStatus.OPEN;
+        LocalDateTime now = LocalDateTime.now();
+        TaskList taskList = taskListRepository.findById(taskListId).orElseThrow(
+                () -> new IllegalArgumentException("Invalid task list ID provided!")
+        );
+
+        return taskRepository.save(new Task(
+                null,
+                task.getTitle(),
+                task.getDescription(),
+                task.getDueDate(),
+                now, now,taskList,taskstatus,taskPriority
+        ));
     }
 }
